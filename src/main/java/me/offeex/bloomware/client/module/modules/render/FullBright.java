6b611d7d60fdc86fc2fe58904581828cecb8a3fb
Cephetir <tupoluboy@gmail.com>
2021-08-13 11:10:34 +0300

    фуллбрайт

--- src/main/java/me/offeex/bloomware/client/module/modules/render/FullBright.java
+++ src/main/java/me/offeex/bloomware/client/module/modules/render/FullBright.java
@@ -7,19 +7,31 @@ public class FullBright extends Module {
 
     public FullBright() {
         super("FullBright", "Boosts your brightness", Category.RENDER, false,
+                //Гамма от 3 до 16 одна и таже и НЕ МЕНЯЕТСЯ вопрос: нахуя так много?
                 new NumberSetting("Gamma", 16, 1, 16, 1));
     }
 
     private double oldBrightness;
+    private double newBrightness;
 
     @Override
     public void onEnable() {
         oldBrightness = mc.options.gamma;
-        mc.options.gamma = ((NumberSetting) this.getSetting(0)).getValue();
+        newBrightness = ((NumberSetting) this.getSetting(0)).getValue();
     }
 
     @Override
     public void onDisable() {
         mc.options.gamma = oldBrightness;
+    }
+
+    @Override
+    public void onTick() {
+        if(this.isEnabled()) {
+            newBrightness = ((NumberSetting) this.getSetting(0)).getValue();
+            if (mc.options.gamma != newBrightness) {
+                mc.options.gamma = newBrightness;
+            }
+        }
     }
 }
