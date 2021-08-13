2c4914cffb9ea60ad900cb36ce6f365946290c33
dion <di0nik.gd@gmail.com>
2021-08-13 11:28:40 +0300

    Added "ViewSent" & "ViewReceived" settings for PacketLogger

--- src/main/java/me/offeex/bloomware/client/module/modules/misc/PacketLogger.java
+++ src/main/java/me/offeex/bloomware/client/module/modules/misc/PacketLogger.java
@@ -4,19 +4,28 @@ import com.google.common.eventbus.Subscribe;
 import me.offeex.bloomware.api.event.events.EventPacket;
 import me.offeex.bloomware.client.command.CommandManager;
 import me.offeex.bloomware.client.module.Module;
+import me.offeex.bloomware.client.setting.Setting;
 
 public class PacketLogger extends Module {
+
+    private final Setting<Boolean> receive = register("ViewReceived", true);
+    private final Setting<Boolean> send = register("ViewSent", true);
+
     public PacketLogger() {
         super("PacketLogger", "", Category.MISC, true);
     }
 
     @Subscribe
     public void onPacketReceive(EventPacket.Receive eventPacket) {
-        CommandManager.addChatMessage("Received packet -> " + eventPacket.getPacket().toString());
+        if (receive.getValue()) {
+            CommandManager.addChatMessage("Received packet -> " + eventPacket.getPacket().toString());
+        }
     }
 
     @Subscribe
     public void onPacketSend(EventPacket.Send event) {
-        CommandManager.addChatMessage("Sent packet -> " + event.getPacket().toString());
+        if (send.getValue()) {
+            CommandManager.addChatMessage("Sent packet -> " + event.getPacket().toString());
+        }
     }
 }
