907836728a32e2739b617f224625ebf812ad94e6
cattyngmd <cvenby@gmail.com>
2021-08-15 19:16:52 +0300

    ваш айкью 7, вы bloomware dev

--- src/main/java/me/offeex/bloomware/client/gui/impl/hud/HUD.java
+++ src/main/java/me/offeex/bloomware/client/gui/impl/hud/HUD.java
@@ -8,6 +8,9 @@ import me.offeex.bloomware.client.module.Module;
 import me.offeex.bloomware.client.module.ModuleManager;
 import net.minecraft.client.MinecraftClient;
 
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
 public class HUD {
 
     MinecraftClient mc = MinecraftClient.getInstance();
@@ -24,7 +27,7 @@ public class HUD {
         RainbowUtil Rainbow = new RainbowUtil();
         int l_I = 0;
         for (Module module : ModuleManager.getModules()) {
-            if (module.isEnabled() && !module.isHidden()) {
+            if (module.isEnabled() && !module.isHidden() && module.shown.getValue()) {
                 mc.textRenderer.drawWithShadow(e.matrix, module.getName(), windowX - mc.textRenderer.getWidth(module.getName()) - 3, windowY, Rainbow.GetRainbowColorAt(l_I), true);
                 windowY += -10;
                 l_I += 15;
