84dc2800a78c5b70507f4f05a30e00e410997b3d
cattyngmd <cvenby@gmail.com>
2021-08-15 19:41:30 +0300

    фикс костыля

--- src/main/java/me/offeex/bloomware/client/module/modules/hud/Fps.java
+++ src/main/java/me/offeex/bloomware/client/module/modules/hud/Fps.java
@@ -3,6 +3,7 @@ package me.offeex.bloomware.client.module.modules.hud;
 import me.offeex.bloomware.Bloomware;
 import me.offeex.bloomware.api.util.ColorUtils;
 import me.offeex.bloomware.client.module.Module;
+import me.offeex.bloomware.api.traits.IMixinMinecraftClient;
 import net.minecraft.client.util.math.MatrixStack;
 
 public class Fps extends Module {
@@ -13,8 +14,8 @@ public class Fps extends Module {
     @Override
     public void draw(MatrixStack stack, int mouseX, int mouseY, float tickDelta) {
         if (isEnabled()) {
-            width = Bloomware.sFontRenderer.getStringWidth("FPS: " + mc.fpsDebugString.split(" ")[0], Bloomware.sFontRenderer.getFontsize()) + 8;
-            Bloomware.sFontRenderer.drawString("FPS: " + mc.fpsDebugString.split(" ")[0], x + 4, y + 4, ColorUtils.getHudColor().getRGB(), true);
+            width = Bloomware.sFontRenderer.getStringWidth("FPS: " + ((IMixinMinecraftClient)mc).fps(), Bloomware.sFontRenderer.getFontsize()) + 8;
+            Bloomware.sFontRenderer.drawString("FPS: " + ((IMixinMinecraftClient)mc).fps(), x + 4, y + 4, ColorUtils.getHudColor().getRGB(), true);
         }
     }
 }
